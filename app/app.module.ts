ecf2e6839c5dbf32e8896e9980a7883709eaa229
bderudder <bjorn.derudder@gmail.com>
2016-11-28 16:19:54 +0100

    added the http module to app and get all the right modules

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule} from "@angular/http";
+import {FormsModule} from "@angular/forms";
 
 import { LoginComponent } from 'app/components/login.component';
 
 import { AppComponent }  from './app.component';
-import {AppRoutingModule} from "./app.routing";
+import { AppRoutingModule } from "./app.routing";
+import { UserService } from "./Services/user.service";
 
 @NgModule({
-    imports:      [ BrowserModule, AppRoutingModule ],
+    imports:      [ BrowserModule, AppRoutingModule, HttpModule, FormsModule ],
     declarations: [ AppComponent, LoginComponent ],
-    bootstrap:    [ AppComponent ]
+    bootstrap:    [ AppComponent ],
+    providers: [UserService]
 })
 export class AppModule { }
