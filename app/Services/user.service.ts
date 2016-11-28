c03097750f0ef3a9e339a3d09b65b38ce5082d66
bderudder <bjorn.derudder@gmail.com>
2016-11-28 20:51:31 +0100

    finalizing user service

--- app/Services/user.service.ts
+++ app/Services/user.service.ts
@@ -1,46 +1,66 @@
 import { Injectable } from '@angular/core';
-import {Http, Headers, URLSearchParams} from '@angular/http';
+import { Http, Headers, URLSearchParams } from '@angular/http';
 
-import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/toPromise'; //to support toPromise
 
 import { User } from '../models/user';
 
 @Injectable()
 export class UserService {
-
-    private apiUrl = 'https://givtapidebug.azurewebsites.net/';
+    //this has to become environment variable in story 2461
+    private apiUrl = 'https://givtapidebug.azurewebsites.net/oauth2/token';
 
     constructor(private http: Http){
-        this.loggedIn = !!localStorage.getItem('auth_token');
+        if(localStorage.getItem('access_token'))
+            this.loggedIn = !!localStorage.getItem('access_token');
+        else if(sessionStorage.getItem('access_token'))
+            this.loggedIn = !!sessionStorage.getItem('access_token');
+        console.log(this.loggedIn);
     }
 
     loggedIn: boolean = false;
     user: User = new User();
 
-    login(username: string, password: string){
+    login(username: string, password: string, stayloggedin: boolean = false){
+        //Set the headers
         let headers = new Headers();
         headers.append('Content-Type', 'application/x-www-form-urlencoded');
 
+        //set the x-www-form-urlencoded parameters
         let urlSearchParams = new URLSearchParams();
         urlSearchParams.append('grant_type', 'password');
         urlSearchParams.append('userName', username);
         urlSearchParams.append('password', password);
+        //set to string
         let body = urlSearchParams.toString();
 
+
+        //do the http call
         return this.http
             .post(
-                this.apiUrl+"oauth2/token",
+                this.apiUrl,
                 body,
                 { headers }
             )
             .toPromise()
             .then(res => {
                 if(res.json().access_token){
-                    localStorage.setItem('access_token', res.json().access_token);
+                    if(stayloggedin){
+                        localStorage.setItem('access_token', res.json().access_token);
+                    }else{
+                        sessionStorage.setItem('access_token', res.json().access_token);
+                    }
+                    return true;
                 }
                 else{
-                    return;
+                    return false;
                 }
             })
+    }
+
+    logout(){
+        sessionStorage.clear();
+        localStorage.clear();
+        this.loggedIn = false;
     }
 }
