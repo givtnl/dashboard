dbc992868db59e694d5e495b69d2c8fce1edfadd
bderudder <bjorn.derudder@gmail.com>
2016-11-28 21:42:21 +0100

    login component is ready next step is implemting routing and going to dashboard, logging in fully works

--- app/components/login.component.ts
+++ app/components/login.component.ts
@@ -10,36 +10,49 @@ import { UserService } from '../Services/user.service';
 export class LoginComponent  {
 
     passwordHidden: boolean;
-    eyeColor:string;
+    eyeColor: string;
+    userName: string;
+    password: string;
+    stay_loggedin: boolean;
+
+    //Setting ready for mulitlingual
+    error_message: string;
+    email_placeholder: string = "Email adres";
+    password_placeholder: string = "Wachtwoord";
+    button_text: string = "inloggen";
+    stayloggedin_text: string = "ingelogd blijven";
 
     constructor(private userService: UserService){
         this.userService = userService;
         this.passwordHidden = true;
-        this.eyeColor = "#2E2957"
+        this.eyeColor = "#BCB9C9";
     }
 
     login(){
         this.error_message = "";
         if(!this.userName || !this.password){
-            this.error_message = "Vul de velden in";
+            this.error_message = "Vul de velden in"; //multilingual
             return;
         }
-        this.userService.login(this.userName, this.password, this.stayloggedin)
+        this.userService.login(this.userName, this.password, this.stay_loggedin)
             .then(resp => {
+                //event emitter and route navigating
                 console.log(resp);
+                console.log("you are logged in.");
             },
-                error => this.error_message = "Verkeerd wachtwoord, probeer opnieuw"
+                error => this.error_message = "Verkeerd wachtwoord of username, probeer opnieuw"//multilingual
             );
     }
 
     showPass(){
         if(this.passwordHidden){
             this.passwordHidden = false;
-            document.getElementById("")
-            console.log("aight");
+            document.getElementById("pass").type = "text";
+            this.eyeColor = "#41C98E";
         }else{
             this.passwordHidden = true;
-            console.log("nope");
+            document.getElementById("pass").type = "password";
+            this.eyeColor = "#BCB9C9";
         }
     }
 }
