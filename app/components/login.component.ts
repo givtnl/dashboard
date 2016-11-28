70571ba7ae2f43217959b55f90592796eeb827bb
bderudder <bjorn.derudder@gmail.com>
2016-11-28 16:20:12 +0100

    updated the login component with two fields and a button

--- app/components/login.component.ts
+++ app/components/login.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
 
+import { UserService } from '../Services/user.service';
+
 @Component({
     selector: 'login',
     templateUrl: 'app/html/login.component.html',
 })
 export class LoginComponent  {
+
+    constructor(private userService: UserService){
+        this.userService = userService;
+    }
+
+    login(){
+        this.userService.login(this.userName, this.password)
+            .then(data => {
+                console.log(data);
+            });
+    }
+
 }
