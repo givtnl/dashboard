9f21c97a985aae124e1d5b728042839a495fc73d
bderudder <bjorn.derudder@gmail.com>
2016-11-28 16:46:19 +0100

    getting css of the login component a little "OK"

--- app/components/login.component.ts
+++ app/components/login.component.ts
@@ -5,6 +5,7 @@ import { UserService } from '../Services/user.service';
 @Component({
     selector: 'login',
     templateUrl: 'app/html/login.component.html',
+    styleUrls: ['./app/css/login.component.css']
 })
 export class LoginComponent  {
 
