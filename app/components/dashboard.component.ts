f61e51e402cec28c1ee1414bc114fab3e4f8260f
bderudder <bjorn.derudder@gmail.com>
2016-11-29 15:53:36 +0100

    First card is ready only multilingual needed

--- app/components/dashboard.component.ts
+++ app/components/dashboard.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 import { Card } from '../models/card';
-import {ApiClientService} from "../services/api-client.service";
+import { ApiClientService } from "../services/api-client.service";
 
 @Component({
     selector: 'my-dashboard',
@@ -11,26 +12,42 @@ import {ApiClientService} from "../services/api-client.service";
 export class DashboardComponent implements OnInit{
     cards: Card[] = [];
     lastSundayCard: Card = new Card();
+    isSafari: boolean;
 
     constructor(private apiService: ApiClientService){
-
+        this.isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
     }
 
     ngOnInit() : void{
         //collect sum of last sunday
         //still have to collect the date of the last sunday
         //cards will come ...
-        this.apiService.getData("OrgAdminView/Givts/?DateBegin=11-28-2016")
+
+        //last sunday card
+        this.fetchLastSundayGivts();
+    }
+
+    fetchLastSundayGivts(){
+        let date = new Date();
+        let day = date.getUTCDay();
+        let lastSunday = new Date(date - day*1000*60*60*24);
+        let lastSundayDate = lastSunday.getUTCMonth()+1 + "-" + lastSunday.getUTCDate() + "-" + lastSunday.getUTCFullYear();
+        let dateBegin =  lastSundayDate + " 00:00:00";
+        let dateEnd = lastSundayDate + " 23:59:59";
+
+        this.apiService.getData("OrgAdminView/Givts/?DateBegin="+dateBegin+"&DateEnd="+dateEnd)
             .then(resp =>
             {
                 let collectSum = 0;
                 for(let givt in resp){
                     collectSum = collectSum + resp[givt].Amount;
                 }
-                this.lastSundayCard.value = collectSum.toString();
+                this.lastSundayCard.value = "€ " + (this.isSafari ? collectSum.toFixed(2) : collectSum.toLocaleString(navigator.language,{minimumFractionDigits: 2}));/*this.isSafari ? this.dayAmount.toFixed(2) : this.dayAmount.toLocaleString(navigator.language,{minimumFractionDigits: 2});*/
                 this.lastSundayCard.title = "last sunday";
+                let datePipe = new DatePipe();
+                this.lastSundayCard.subtitle = datePipe.transform(lastSundayDate, 'dd MMMM yyyy');
+                //this.lastSundayCard.subtitle = lastSundayDate;
                 this.lastSundayCard.footer = "given";
-                this.lastSundayCard.subtitle = "hoi";
 
                 this.cards.push(this.lastSundayCard);
             });
