14786faed2f388f96677053900076e4de91fb6b6
bderudder <bjorn.derudder@gmail.com>
2016-11-29 14:44:13 +0100

    finally added all the modules to the appmodule!!

--- app/app.module.ts
+++ app/app.module.ts
@@ -7,14 +7,18 @@ import { LoginComponent } from 'app/components/login.component';
 
 import { AppComponent }  from './app.component';
 import { AppRoutingModule } from "./app.routing";
-import { UserService } from "./Services/user.service";
+import { UserService } from "./services/user.service";
+import { NavigationComponent } from "./components/navigation.component";
+import {Router} from "@angular/router";
 import {LoggedInGuard} from "./guards/logged-in.guard";
 import {LoginComponentGuard} from "./guards/login.component.guard";
+import {ApiClientService} from "./services/api-client.service";
+import {DashboardComponent} from "./components/dashboard.component";
 
 @NgModule({
-    imports:      [ BrowserModule, AppRoutingModule, HttpModule, FormsModule ],
-    declarations: [ AppComponent, LoginComponent ],
+    imports:      [ BrowserModule, AppRoutingModule, HttpModule, FormsModule, AppRoutingModule ],
+    declarations: [ AppComponent, LoginComponent, NavigationComponent, DashboardComponent ],
     bootstrap:    [ AppComponent ],
-    providers: [UserService]
+    providers: [ UserService, ApiClientService, LoggedInGuard, LoginComponentGuard ]
 })
 export class AppModule { }
