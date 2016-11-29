ec1037abc2f3f5c77fbf9f140afd898c41dc7c9a
Lennie Stockman <lenniestockman@Givts-MacBook-Pro.local>
2016-11-29 15:05:26 +0100

    init translateservice

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
 
+import {TranslateService} from 'ng2-translate';
 @Component({
     selector: 'my-app',
-    templateUrl: 'app/html/app.component.html',
+    templateUrl: 'app/html/app.component.html'
 })
 export class AppComponent  {
+    constructor(translate: TranslateService) {
+        // this language will be used as a fallback when a translation isn't found in the current language
+        translate.setDefaultLang('nl');
+
+        // the lang to use, if the lang isn't available, it will use the current loader to get them
+        //todo: set lang according to navigator.language
+        translate.use('nl');
+    }
 }
