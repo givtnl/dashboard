f68161d9c9fbeb626b9a69d9a0f5b0e143a4f304
Lennie Stockman <lenniestockman@hotmail.com>
2016-12-08 14:05:33 +0100

    Fixed language logic

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -8,19 +8,17 @@ import {TranslateService} from 'ng2-translate';
 export class AppComponent  {
   constructor(translate: TranslateService) {
     // this language will be used as a fallback when a translation isn't found in the current language
-    translate.setDefaultLang('nl');
+    //translate.setDefaultLang('nl');
 
     //supported languages todo: add languages you wish to support
     translate.addLangs(["nl","en"]);
 
     let languages = translate.getLangs();
     let browserLanguage = navigator.language.substr(0,2);
-    for(let lang in languages){
-      if(languages[lang] === browserLanguage){
-        translate.use(languages[lang]);
-      }else{
-        translate.use("nl");
+      if(languages.indexOf(browserLanguage) !== -1){
+          translate.use(browserLanguage);
+      } else {
+          translate.use("nl");
       }
-    }
   }
 }
