9799e8cd2f72eb83ae7939d719f714f1d7cd077c
bderudder <bjorn.derudder@gmail.com>
2016-12-12 16:15:08 +0100

    Added post data and delete to the apiService

--- src/app/services/api-client.service.ts
+++ src/app/services/api-client.service.ts
@@ -16,6 +16,60 @@ export class ApiClientService {
 
     }
 
+    delete(path: string){
+        let accessToken = this.userService.getAccessToken();
+        if(!accessToken){
+            return;
+        }
+        let headers = new Headers();
+        headers.append('authorization', 'Bearer '+ accessToken);
+
+        return this.http
+            .delete(
+            this.apiUrl + path,
+            { headers }
+            )
+            .toPromise()
+            .then(res => {
+                return res;
+            }).catch(
+                err => console.log(err);
+            )
+    }
+
+    postData(path: string, body: any){
+        let accessToken = this.userService.getAccessToken();
+        if(!accessToken){
+            return;
+        }
+
+        let json = JSON.stringify(body);
+
+        //Set the headers
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        headers.append('authorization', 'Bearer '+ accessToken);
+
+        return this.http
+            .post(
+                this.apiUrl + path,
+                json,
+                { headers }
+            )
+            .toPromise()
+            .then(res => {
+                try {
+                    return res.json();
+                } catch (e) {
+                    return res._body;
+                }
+            }).catch(err =>  {
+                if(err.status === 403){
+                    this.router.navigate(['unauthorized']);
+                }
+            })
+    }
+
     getData(path: string){
         //check for token
         let accessToken = this.userService.getAccessToken();
@@ -41,7 +95,7 @@ export class ApiClientService {
                     return res._body;
                 }
             }).catch(err =>  {
-                if(err.status === 403){
+                if(err.status === 403 || err.status === 401){
                     this.router.navigate(['unauthorized']);
                 }
             })
