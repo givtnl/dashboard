21f98653f10b1d615ffde2548083954603e69886
Bjorn Derudder <bjorn.derudder@gmail.com>
2017-03-08 17:20:21 +0100

    payouts can now be viewed in detail

--- src/app/components/payouts.component.ts
+++ src/app/components/payouts.component.ts
@@ -12,8 +12,15 @@ import {Payout} from "../models/payout";
 export class PayoutsComponent implements OnInit{
 
     payouts : Payout[] = [];
+    isSafari: boolean;
+
+    transactionCost = 0.08;
+    mandateCost = 0.125;
+
 
     constructor(private apiService: ApiClientService) {
+        this.isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
     }
 
     ngOnInit(){
@@ -23,14 +30,39 @@ export class PayoutsComponent implements OnInit{
             {
                 this.payouts = resp;
                 for(let i in this.payouts){
-                    this.payouts[i].BeginDate = datepipe.transform(this.payouts[i].BeginDate, "d MMMM y");
-                    this.payouts[i].EndDate = datepipe.transform(this.payouts[i].EndDate, "d MMMM y");
-                    this.payouts[i].hidden = false;
+                    let x = this.payouts[i];
+                    x.BeginDate = datepipe.transform(this.payouts[i].BeginDate, "d MMMM y");
+                    x.EndDate = datepipe.transform(this.payouts[i].EndDate, "d MMMM y");
+                    x.hidden = true;
+
+                    x.TransactionCost =  this.displayValue(x.TransactionCount * this.transactionCost);
+                    x.MandateCost =  this.displayValue(x.MandateCostCount * this.mandateCost);
+                    x.TotalTransactionCost = this.displayValue(x.TransactionCount * this.transactionCost + x.MandateCostCount * this.mandateCost);
+                    x.activeRow = 1;
                 }
             });
     }
 
-    openDetail(x){
+    displayValue(x)
+    {
+        return "€ " + (this.isSafari ? (x).toFixed(2) : (x).toLocaleString(navigator.language,{minimumFractionDigits: 2,maximumFractionDigits:2}));
+    }
+
+    openPayout(x){
+        for(let i in this.payouts)
+        {
+            this.payouts[i].hidden = true;
+        }
+        x.hidden = !x.hidden;
         console.log(x);
+    }
+
+    closePayout(x){
+        x.hidden = !x.hidden;
+    }
+
+    selectRow(x, y)
+    {
+        x.activeRow = y;
     }
 }
