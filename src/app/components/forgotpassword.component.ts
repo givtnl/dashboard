e99c1089b14cc06bdaf97a4d387b7975430cae22
Lennie Stockman <lenniestockman@Lennies-MacBook-Pro.local>
2017-10-20 15:02:27 +0200

    changed indenting and change term rather than printing response from server

--- src/app/components/forgotpassword.component.ts
+++ src/app/components/forgotpassword.component.ts
@@ -12,151 +12,154 @@ import {TranslateService} from 'ng2-translate';
 declare var params: any;
 
 @Component({
-    selector: 'forgotpassword',
-    templateUrl: '../html/forgotpassword.component.html',
-    styleUrls: ['../css/forgotpassword.component.css']
+  selector: 'forgotpassword',
+  templateUrl: '../html/forgotpassword.component.html',
+  styleUrls: ['../css/forgotpassword.component.css']
 })
 export class ForgotPasswordComponent  implements OnInit{
-    passwordHidden: boolean;
-    eyeColor: string;
-    userName: string;
-    password: string;
-    stay_loggedin: boolean;
-    error_message: string;
+  passwordHidden: boolean;
+  eyeColor: string;
+  userName: string;
+  password: string;
+  stay_loggedin: boolean;
+  error_message: string;
 
-    /*  booleans for flow  */
-    email_given: boolean = false;
-    saved : boolean = false;
-    no_email_given : boolean = false;
-    wrong: boolean = false;
-    email_sent: boolean = false;
-    coming_from_app: boolean = false;
-    queryParams: any;
-    /*  ------  */
+  /*  booleans for flow  */
+  email_given: boolean = false;
+  saved : boolean = false;
+  no_email_given : boolean = false;
+  wrong: boolean = false;
+  email_sent: boolean = false;
+  coming_from_app: boolean = false;
+  queryParams: any;
+  /*  ------  */
 
-    disabled : boolean = false;
+  disabled : boolean = false;
 
-    email: string;
-    token: string;
+  email: string;
+  token: string;
 
-    mobile: boolean;
+  mobile: boolean;
 
 
-    translate: TranslateService;
-    constructor(private userService: UserService, private router: Router, translate:TranslateService, private route : ActivatedRoute){
-        this.userService = userService;
-        this.passwordHidden = true;
-        this.eyeColor = "#BCB9C9";
-        this.translate = translate;
-    }
+  translate: TranslateService;
+  constructor(private userService: UserService, private router: Router, translate:TranslateService, private route : ActivatedRoute){
+    this.userService = userService;
+    this.passwordHidden = true;
+    this.eyeColor = "#BCB9C9";
+    this.translate = translate;
+  }
 
-    ngOnInit(){
-        this.mobile =  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-        this.resetNav();
-        this.route
-            .queryParams
-            .subscribe(params => {
-                if(params.email){
-                    this.email_given = true;
-                    this.email = params.email;
-                }else{
-                    this.no_email_given = true;
-                }
-                if(params.code){
-                    this.token = params.code;
-                }
-                if(params.e){
-                    this.userName = params.e;
-                }
-                if(params.app){
-                    this.coming_from_app = (params.app === "true");
-                }
-                this.queryParams = params;
-            });
-    }
-
-    requestPass(){
-        this.disabled = true;
-        if(!this.userName)
-        {
-            this.disabled = false;
-            return;
-        }
-        this.userService.requestNewPass(this.userName)
-            .then(resp => {
-                this.disabled = false;
-                if(resp.ok == true)
-                {
-                    this.resetNav();
-                    this.email_sent = true;
-                    this.translate.get('PW_RequestedPW').subscribe((res: string) => {
-                        this.error_message = res;
-                    });
-                }
-            });
-    }
-
-    save(){
-        this.disabled = true;
-        this.error_message = null;
-        this.userService.saveNewPass(this.email, this.token, this.password)
-            .then(res => {
-                this.disabled = false;
-                this.resetNav();
-                if(res.ok)
-                {
-                    console.log(res);
-                    this.saved = true;
-                }else{
-                    this.wrong = true;
-                }
-            })
-            .catch(err =>{
-                this.disabled = false;
-                this.resetNav();
-                this.wrong = true;
-                this.error_message = err._body;
-                console.log(err);
-            })
-    }
-
-    retry(){
-        this.wrong = false;
-        this.route
-            .queryParams
-            .subscribe(params => {
-                this.resetNav();
-                if(params.email){
-                    this.email_given = true;
-                    this.email = params.email;
-                }else{
-                    this.no_email_given = true;
-                }
-                if(params.code){
-                    this.token = params.code;
-                }
-                this.queryParams = params;
-            });
-    }
-
-    showPass(){
-        if(this.passwordHidden){
-            this.passwordHidden = false;
-            (<HTMLInputElement>document.getElementById("pass")).type = "text";
-            this.eyeColor = "#41C98E";
+  ngOnInit(){
+    this.mobile =  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    this.resetNav();
+    this.route
+      .queryParams
+      .subscribe(params => {
+        if(params.email){
+          this.email_given = true;
+          this.email = params.email;
         }else{
-            this.passwordHidden = true;
-            (<HTMLInputElement>document.getElementById("pass")).type = "password";
-            this.eyeColor = "#BCB9C9";
+          this.no_email_given = true;
         }
-    }
+        if(params.code){
+          this.token = params.code;
+        }
+        if(params.e){
+          this.userName = params.e;
+        }
+        if(params.app){
+          this.coming_from_app = (params.app === "true");
+        }
+        this.queryParams = params;
+      });
+  }
 
-    resetNav(){
-        this.saved = false;
-        this.email_given = false;
-        this.no_email_given = false;
-        this.wrong = false;
-        this.email_sent = false;
+  requestPass(){
+    this.disabled = true;
+    if(!this.userName)
+    {
+      this.disabled = false;
+      return;
     }
+    this.userService.requestNewPass(this.userName)
+      .then(resp => {
+        this.disabled = false;
+        if(resp.ok == true)
+        {
+          this.resetNav();
+          this.email_sent = true;
+          this.translate.get('PW_RequestedPW').subscribe((res: string) => {
+            this.error_message = res;
+          });
+        }
+      });
+  }
+
+  save(){
+    this.disabled = true;
+    this.error_message = null;
+    this.userService.saveNewPass(this.email, this.token, this.password)
+      .then(res => {
+        this.disabled = false;
+        this.resetNav();
+        if(res.ok)
+        {
+          console.log(res);
+          this.saved = true;
+        }else{
+          this.wrong = true;
+        }
+      })
+      .catch(err =>{
+        this.disabled = false;
+        this.resetNav();
+        this.wrong = true;
+
+        this.translate.get('PW_PWPolicy').subscribe((res: string) => {
+          this.error_message = res;
+        });
+        console.log(err);
+      })
+  }
+
+  retry(){
+    this.wrong = false;
+    this.route
+      .queryParams
+      .subscribe(params => {
+        this.resetNav();
+        if(params.email){
+          this.email_given = true;
+          this.email = params.email;
+        }else{
+          this.no_email_given = true;
+        }
+        if(params.code){
+          this.token = params.code;
+        }
+        this.queryParams = params;
+      });
+  }
+
+  showPass(){
+    if(this.passwordHidden){
+      this.passwordHidden = false;
+      (<HTMLInputElement>document.getElementById("pass")).type = "text";
+      this.eyeColor = "#41C98E";
+    }else{
+      this.passwordHidden = true;
+      (<HTMLInputElement>document.getElementById("pass")).type = "password";
+      this.eyeColor = "#BCB9C9";
+    }
+  }
+
+  resetNav(){
+    this.saved = false;
+    this.email_given = false;
+    this.no_email_given = false;
+    this.wrong = false;
+    this.email_sent = false;
+  }
 }
 
