bf9fc205b6de4347e756fa1c0e76837a5327642c
Mike Pattyn <>
2018-04-23 16:04:53 +0200

    sliding sidebar ( mobile devices )

--- src/app/components/navigation.component.ts
+++ src/app/components/navigation.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { UserService } from 'app/services/user.service';
 import {Router} from "@angular/router";
+import {forEach} from "@angular/router/src/utils/collection";
+import {childOfKind} from "tslint";
 
 @Component({
     selector: 'my-navigation',
     templateUrl: '../html/navigation.component.html',
     styleUrls: ['../css/navigation.component.css']
 })
-export class NavigationComponent implements OnInit, AfterViewInit {
+export class NavigationComponent implements OnInit {
     userService: UserService;
 
     showMandateLink = false;
     showDashboardItems = true;
+    toggleSidebar = false;
     currentCollectGroup: any = {Name:"", GUID:""};
     collectGroups : Array<any> = null;
     constructor(userService: UserService, private router: Router) {
@@ -34,12 +37,29 @@ export class NavigationComponent implements OnInit, AfterViewInit {
           }
       }
     }
-    ngAfterViewInit() {
-      console.log("dit werkt");
-    }
 
     logout(){
         this.userService.logout();
         this.router.navigate(['']);
+    }
+
+    toggleFullSidebar(){
+      this.toggleSidebar = ! this.toggleSidebar;
+      let sidebar = document.getElementById('sidebar');
+      let sidebarList = document.getElementsByClassName("sidebar-title");
+      let sidebarGivtLogo = document.getElementById("givt-sidebar-logo");
+      if(this.toggleSidebar){
+        for(let i = 0; i < sidebarList.length; ++i){
+          sidebar.setAttribute('style','width: 141px; padding-right: 20px');
+          sidebarList[i].setAttribute('style', 'display:inline-block !important;');
+        }
+      }
+      else {
+        for(let i = 0; i < sidebarList.length; ++i){
+          sidebar.setAttribute('style','width: 52px; padding-right: 0px');
+          sidebarList[i].setAttribute('style', 'display:none !important');
+        }
+      }
+
     }
 }
