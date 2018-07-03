206172b624e1728245944e9cfe974b3eb26a9bed
Lennie <lenniestockman96@gmail.com>
2018-07-03 14:02:07 +0200

    check wether the allocation belongs to this allocation

--- src/app/components/assign.component.ts
+++ src/app/components/assign.component.ts
@@ -529,6 +529,7 @@ export class AssignComponent implements OnInit {
       let currentBucket = buckets.filter((b) => b.startTime.getTime() == noFixed[x]['dtBegin'].getTime() && b.endTime.getTime() ==noFixed[x]['dtEnd'].getTime())[0];
       currentBucket.transactions.push(noFixed[x]);
     }
+    let currentCollectGroupId = JSON.parse(this.dataService.getData("CurrentCollectGroup")).GUID;
     for(let i = 0; i < buckets.length; i++) {
       let event = new MyEvent();
       event.id = buckets[i].allocationId;
@@ -536,7 +537,7 @@ export class AssignComponent implements OnInit {
       event.start = buckets[i].startTime;
       event.end = buckets[i].endTime;
       // STATUS == 0 MEANS ALLOC IS ASSIGNED FOR OTHER COLLECTGR
-      event.className = buckets[i].transactions[0].Status == 0 ? "allocation-disabled" : "allocation";
+      event.className = buckets[i].transactions[0].CollectGroupId == currentCollectGroupId ? "allocation" : "allocation-disabled";
       event.allocated = true;
       event.amount = null;
       event.transactions = buckets[i].transactions;
