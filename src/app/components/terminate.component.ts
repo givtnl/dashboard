0cabee223e0df97ff7cc230901299a210a901cf4
Lennie <lenniestockman96@gmail.com>
2018-07-12 15:15:09 +0200

    terminating user

--- src/app/components/terminate.component.ts
+++ src/app/components/terminate.component.ts
@@ -1,6 +1,8 @@
 import { OnInit, Component } from "../../../node_modules/@angular/core";
 import { TranslateService } from "../../../node_modules/ng2-translate";
 import { DataService } from "../services/data.service";
+import {Router, ActivatedRoute} from '@angular/router';
+import { ApiClientService } from "../services/api-client.service";
 
 @Component({
     selector: 'terminate',
@@ -15,12 +17,18 @@ export class TerminateComponent implements OnInit {
         this.terminateState = TerminateState.Undecided
     }
 
-    constructor(private translateService: TranslateService, private dataService: DataService) {
+    constructor(private translateService: TranslateService, private dataService: DataService, private apiClientService: ApiClientService, private route : ActivatedRoute) {
         this.terminateState = TerminateState.Undecided;
     }
 
     terminate() {
         this.terminateState = TerminateState.Terminated;
+        let guid = this.route.snapshot.queryParams["guid"];
+        let token = this.route.snapshot.queryParams["token"]
+        this.apiClientService.delete("v2/users/" + guid + "?token=" + token)
+        .then(resp => {
+            console.log("User is deleted");
+        });
     }
 
     cancelTerminate() {
