8dd76fa1b9dedcb592cd1c48d348aaa6f9329476
Mike Pattyn <mike@givtapp.net>
2018-12-13 13:24:20 +0100

    set locale in constructor of DatePipe

--- src/app/pipes/iso.datepipe.ts
+++ src/app/pipes/iso.datepipe.ts
@@ -1,9 +1,12 @@
 import {DatePipe} from '@angular/common';
-import {Pipe} from "@angular/core";
+import {Pipe, PipeTransform} from "@angular/core";
 import {sprintf} from "sprintf-js"
 
 @Pipe({ name: 'isoDate' })
-export class ISODatePipe extends DatePipe {
+export class ISODatePipe extends DatePipe implements PipeTransform {
+    constructor(){
+        super(navigator.language.includes('nl') ? 'nl-NL' : 'en-GB');
+    }
     toISODateUTC(value: Date) : string {
         return sprintf("%04i-%02i-%02iT%02i:%02i:%02i.%03i",
             value.getUTCFullYear(), value.getUTCMonth()+1, value.getUTCDate(),
