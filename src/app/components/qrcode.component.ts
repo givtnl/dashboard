09e7af8f6857156ec98269198f4ca0b73579ab38
Jonas Brabant <jonas@givtapp.net>
2019-05-02 17:09:06 +0200

    QR code flow - complete flow design

--- src/app/components/qrcode.component.ts
+++ src/app/components/qrcode.component.ts
@@ -1,8 +1,10 @@
-import {Component, OnInit} from "@angular/core";
-import {ApiClientService} from "../services/api-client.service";
-import {DataService} from "../services/data.service";
-import {ISODatePipe} from "../pipes/iso.datepipe";
+import { Component, OnInit } from "@angular/core";
+import { ApiClientService } from "../services/api-client.service";
+import { DataService } from "../services/data.service";
+import { ISODatePipe } from "../pipes/iso.datepipe";
 import { UserService } from "app/services/user.service";
+import { THIS_EXPR } from "@angular/compiler/src/output/output_ast";
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'qr-code',
@@ -11,29 +13,63 @@ import { UserService } from "app/services/user.service";
 })
 export class QRCodeComponent implements OnInit {
 
-    constructor(private apiService: ApiClientService, private dataService: DataService, private datePipe: ISODatePipe) {
-        
+	currentQuestionId = 1
+	isChecked = false
+	constructor(private apiService: ApiClientService, private dataService: DataService, private datePipe: ISODatePipe, private router:Router) {
+
 	}
 
-	slideIndex = 1;
+	private fieldArray: Array<{ value: string, placeholder: string }> = [];
+	private newAttribute: any = { placeholder: "Nieuw Collectedoel" };
 
-	next(n) {
-		this.showquestion(this.slideIndex += n);
-	}
-	previous(n) {
-		this.showquestion(this.slideIndex -= n)
+	addFieldValue() {
+		this.fieldArray.push(this.newAttribute)
+		this.newAttribute = { placeholder: "Nieuw Collectedoel" };
 	}
 
-	showquestion(n) {
-		var slides = document.getElementsByClassName("questions-component");
+	deleteFieldValue(index) {
+		this.fieldArray.splice(index, 1);
 	}
-	
+
 	ngOnInit(): void {
-		var myQRQuestion = 0;
+		this.fieldArray.push({ value: "Bouwfonds", placeholder: "" })
+		this.fieldArray.push({ value: "Evenement", placeholder: "" })
+	}
+
+	showNextQuestion() {
+		this.currentQuestionId++
+	}
+
+	showPreviousQuestion() {
+		this.currentQuestionId--
+	}
+
+	checkboxClicked() {
+		this.isChecked = !this.isChecked
+	}
+
+	goHome() {
+		this.router.navigateByUrl('/');
+	}
+
+	skipToEnd() {
+		this.currentQuestionId = 4;
+	}
+
+	submit() {
+		var currentValue = (<HTMLInputElement> document.getElementById('phonenumber')).value;
 
 		
 
-
-
 	}
+
+	keyPressPhonenumber(event: any) {
+		const pattern = /[0-9\+\-\ ]/;
+	
+		let inputChar = String.fromCharCode(event.charCode);
+		if (event.keyCode != 8 && !pattern.test(inputChar)) {
+		  event.preventDefault();
+		}
+	  }
+
 }
