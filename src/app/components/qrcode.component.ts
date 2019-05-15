4f60d1d6e75224a5a0332c857203bfa04ed9ac56
Bjorn Derudder <bjorn.derudder@gmail.com>
2019-05-15 12:20:15 +0200

    no semicolon

--- src/app/components/qrcode.component.ts
+++ src/app/components/qrcode.component.ts
@@ -24,18 +24,19 @@ export class QRCodeComponent {
 	constructor(private translateService :TranslateService, private apiService: ApiClientService, private dataService: DataService, private datePipe: ISODatePipe, private router: Router, private http: Http, private userService: UserService) {
 
 	}
-	public name = "";
-	GenericQR: boolean = false;
-	currentQuestionId: number = 1;
-	fieldArray: string[] = [""];
-	giftPurposes: string[] = [];
+	public name = ""
+	GenericQR: boolean = false
+	currentQuestionId: number = 1
+	fieldArray: string[] = [""]
+	giftPurposes: string[] = []
 	isPhoneValid: boolean = true
 	isEmailValid: boolean = true
 
-	email = this.dataService.getData('UserEmail');
+
+	email = this.dataService.getData('UserEmail')
 	phonenumber = ""
 	comments = ""
-	private newAttribute: string = "";
+	private newAttribute: string = ""
 
 	async showNextQuestion(value: number) {
 		switch (this.currentQuestionId) {
@@ -43,26 +44,26 @@ export class QRCodeComponent {
 				if(this.fieldArray[0] == null) {
 					this.fieldArray.push("")
 				}
-				this.currentQuestionId += value;
-				break;
+				this.currentQuestionId += value
+				break
 			case 3:
-				this.fieldArray = this.fieldArray.filter(element => element.trim() !== "");
+				this.fieldArray = this.fieldArray.filter(element => element.trim() !== "")
 
 				this.fieldArray.forEach((element, index) => {
-					this.fieldArray[index] = element.trim();
+					this.fieldArray[index] = element.trim()
 				})
-				this.currentQuestionId += value;
-				break;
+				this.currentQuestionId += value
+				break
 			case 4:
-				this.checkEmail();
-				this.checkPhoneNumber();
+				this.checkEmail()
+				this.checkPhoneNumber()
 
 				if (this.isEmailValid && this.isPhoneValid) {				
-					var submitok = await this.submit();
+					var submitok = await this.submit()
 					if(submitok==false){
 						this.translateService.get("QRCodeREQ_warning_submitfailed").subscribe((res) => setTimeout(() => {alert(res)}, 200))
 					} else 
-						this.currentQuestionId += value;
+						this.currentQuestionId += value
 				} else {
 					if(!this.isEmailValid && this.isPhoneValid) {
 						this.translateService.get("QRCodeREQ_warning_novalidemail").subscribe((res) => alert(res))
@@ -72,65 +73,66 @@ export class QRCodeComponent {
 						this.translateService.get("QRCodeREQ_warning_novaliddata").subscribe((res) => alert(res))
 					}					
 				}
-				break;
+				break
 			default:
-				this.currentQuestionId += value;
-				break;
+				this.currentQuestionId += value
+				break
 		}
 	}
 	showPreviousQuestion(value: number = 1) {
-		this.currentQuestionId -= value;
+		this.currentQuestionId -= value
 
 		switch (this.currentQuestionId) {
 			case 3:
 				if(this.fieldArray[0] == null) {
 					this.fieldArray.push("")
 				} 
-				break;
+				break
 
 			default:
-				break;
+				break
 		}
 	}
 
 	deleteFieldValue(index) {
-		this.fieldArray.splice(index, 1);
+		this.fieldArray.splice(index, 1)
 	}
 
 	trackByFn(index: any, item: any) {
-		return index;
+		return index
 	}
 
 	async submit() {
-		var submitSuccessfull = false;
+
+		var submitSuccessfull = false
 		let body: QRRequestBody = {email: this.email, phoneNumber: this.phonenumber, collectGoals: this.fieldArray, comments: this.comments}
-		let apiUrl = 'v2/collectgroups/' + this.userService.CurrentCollectGroup.GUID + '/qrcodes';
+		let apiUrl = 'v2/collectgroups/' + this.userService.CurrentCollectGroup.GUID + '/qrcodes'
 
 		await this.apiService.postData(apiUrl, body)
 			.then(resp => {
 				if(Number(resp) > 0) {
-					submitSuccessfull = true;
+					submitSuccessfull = true
 				}
 			})
 			.catch(err => {
-				submitSuccessfull = false;
-			});
-		return submitSuccessfull;
+				submitSuccessfull = false
+			})
+		return submitSuccessfull
 	}
 
 	checkEmail() {
 		var emailField = <HTMLInputElement>document.getElementById('email')
-		this.email = emailField.value;
-		const regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+		this.email = emailField.value
+		const regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
 		this.isEmailValid = regexp.test(this.email)
 
 		return this.isEmailValid
 	}
 
 	checkPhoneNumber() {
-		this.phonenumber = (<HTMLInputElement>document.getElementById('phonenumber')).value;
+		this.phonenumber = (<HTMLInputElement>document.getElementById('phonenumber')).value
 
-		var currentValue = this.phonenumber;
+		var currentValue = this.phonenumber
 		if (
 			(currentValue.length == 10 && (currentValue.charAt(1) == "6" || currentValue.charAt(1) == "4")) ||
 			(currentValue.length == 11 && currentValue.charAt(1) == "7")
@@ -143,32 +145,32 @@ export class QRCodeComponent {
 	}
 
 	flowGeneric() {
-		this.GenericQR = true;
-		var respsonse;
+		this.GenericQR = true
+		var respsonse
 		this.translateService.get("QRCodeREQ_generic").subscribe((res) => respsonse = res)
-		this.fieldArray = [String(respsonse)];
-		this.showNextQuestion(2);
+		this.fieldArray = [String(respsonse)]
+		this.showNextQuestion(2)
 	}
 
 	flowSpecific() {
-		this.GenericQR = false;
-		this.showNextQuestion(1);
+		this.GenericQR = false
+		this.showNextQuestion(1)
 	}
 
 	undoProces() {
 		if (this.GenericQR) {
-			this.showPreviousQuestion(2);
-			this.fieldArray = [""];
+			this.showPreviousQuestion(2)
+			this.fieldArray = [""]
 		}
 		else {
-			this.showPreviousQuestion(1);
+			this.showPreviousQuestion(1)
 		}
 	}
 }
 
 class QRRequestBody {
-	email: string;
-	phoneNumber: string;
-	collectGoals: string[];
-	comments: string;
+	email: string
+	phoneNumber: string
+	collectGoals: string[]
+	comments: string
 }
