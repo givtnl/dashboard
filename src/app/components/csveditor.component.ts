765c58b51da08164456d20fec8b714d6f0b5b100
Jonas Brabant <jonas@givtapp.net>
2019-08-08 14:29:44 +0200

    Added CSV editor component

---
+++ src/app/components/csveditor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-csveditor',
+  templateUrl: '../html/csveditor.component.html',
+  styleUrls: ['../css/csveditor.component.css']
+})
+export class CSVEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
