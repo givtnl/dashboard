df6d23193c69a328349c0a82c11f5b2e8420212c
Mike Pattyn <mike@givtapp.net>
2019-08-21 14:09:28 +0200

    added insert new row via plus control with indexing and added index param to copyrow function

--- src/app/components/collects-scheduler.component.ts
+++ src/app/components/collects-scheduler.component.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormArray, ValidatorFn, FormControl } from '@angular/forms';
-import { environment } from 'environments/environment';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators, FormArray, ValidatorFn } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 import { UserService } from 'app/services/user.service';
 import { isNullOrUndefined } from 'util';
-import { catchError } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { distinctUntilChanged, debounceTime } from 'rxjs/operators';
 
 const GreaterThanDateValidator: ValidatorFn = (formGroup: FormGroup) => {
   var retVal = null
@@ -37,7 +35,7 @@ export class CollectsShedulerComponent implements OnInit {
   public isSuccessfulAllocations = false;
   currentCollectGroupId: string
 
-  constructor(private formBuilder: FormBuilder, private http: HttpClient, private userService: UserService) { }
+  constructor(private formBuilder: FormBuilder, private userService: UserService) { }
 
   ngOnInit() {
     // restore from cache if exists
@@ -54,60 +52,60 @@ export class CollectsShedulerComponent implements OnInit {
       this.addEmptyRow();
     }
     // hook the value change event handler
-    this.form.valueChanges.subscribe(x => localStorage.setItem(this.cacheKey, JSON.stringify(x)));
+    this.form.valueChanges.subscribe(x => {
+      localStorage.setItem(this.cacheKey, JSON.stringify(x))
+    });
   }
 
   public get collectsArray(): FormArray {
     return this.form.get('collects') as FormArray;
   }
 
-  buildSingleForm(scheduler: any = null): FormGroup {
-    return this.formBuilder.group({
-      Id: [scheduler ? scheduler.Id : 0],
+  buildSingleForm(scheduler: any = null, copyId: boolean = false): FormGroup {
+    const form = this.formBuilder.group({
+      Id: [scheduler && copyId ? scheduler.Id : 0],
       dtBegin: [scheduler ? scheduler.dtBegin : null, [Validators.required]],
       dtEnd: [scheduler ? scheduler.dtEnd : null, [Validators.required]],
       Name: [scheduler ? scheduler.Name : null, [Validators.required, Validators.maxLength(50), Validators.minLength(3)]],
       CollectId: [scheduler ? scheduler.CollectId : 1, [Validators.required, Validators.min(1), Validators.max(3)]]
-    }, { validator: GreaterThanDateValidator })
+    }, { validator: GreaterThanDateValidator });
+
+    form.valueChanges
+      .pipe(debounceTime(2000))
+      .pipe(distinctUntilChanged())
+      .subscribe(x => this.upload(x));
+
+    return form;
   }
 
-  addEmptyRow() {
-    var selectedRow = this.collectsArray.controls.find(x => x.touched == true)
-    if (selectedRow) {
-      this.collectsArray.insert(this.collectsArray.controls.indexOf(selectedRow), this.buildSingleForm(selectedRow.value))
-      selectedRow.markAsUntouched()
-    } else {
-      this.collectsArray.push(this.buildSingleForm());
-    }
+  addEmptyRow(index: number = 0) {
+    this.collectsArray.insert(index, this.buildSingleForm());
   }
 
   removeRow(index: number) {
+    const toDeleteFormGroup = this.collectsArray.at(index);
+    if (toDeleteFormGroup.value.id > 0) {
+      //  blabladelete
+      // then.
+    } else {
+      this.removeRowConfirm(index);
+    }
+  }
+
+  removeRowConfirm(index: number) {
     this.collectsArray.removeAt(index);
   }
 
   copyRow(index: number) {
-    this.collectsArray.push(this.buildSingleForm(this.collectsArray.at(index).value))
+    this.collectsArray.insert(index, this.buildSingleForm(this.collectsArray.at(index).value))
   }
 
-  upload() {
-    this.hasOverlapErrors = false;
-    this.isSuccessfulAllocations = false;
-
-    this.http.post(`${environment.apiUrl}/api/v2/allocations/allocation`, this.form.value)
-      .pipe(catchError((error: HttpErrorResponse) => {
-        switch (error.status) {
-          case 409:
-            this.handleConflict(error);
-            break;
-          default:
-            this.handleGenericError();
-        }
-        return Observable.throw(error);
-      }))
-      .subscribe(() => {
-        localStorage.removeItem(this.cacheKey)
-        this.isSuccessfulAllocations = true
-      });
+  upload(row: any) {
+    console.log('row changed ', row);
+    // id if === 0
+    //create
+    //else
+    //update
   }
   handleConflict(error: any) {
     this.hasOverlapErrors = true
