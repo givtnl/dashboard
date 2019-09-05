ccd1a8625cad4f345475896ea9bacfa4edf0942f
Anthony <anthony@boutonanthony.be>
2019-09-05 14:29:11 +0200

    add message when theres an error in either the form or an row

--- src/app/components/collects-scheduler.component.ts
+++ src/app/components/collects-scheduler.component.ts
@@ -10,153 +10,166 @@ import { Observable } from 'rxjs';
 import { ISODatePipe } from 'app/pipes/iso.datepipe';
 import { LoggingService } from 'app/services/logging.service';
 import { compareRows } from 'app/models/collect-scheduler/row-comparer.function';
-
-const GreaterThanDateValidator: ValidatorFn = (formGroup: FormGroup) => {
-  var retVal = null
-
-  const start: Date = formGroup.value.dtBegin;
-  const end: Date = formGroup.value.dtEnd;
-
-  if (!isNullOrUndefined(start) && !isNullOrUndefined(end) && (start >= end)) {
-    retVal = {
-      invalidDate: true
-    }
-  }
-
-  return retVal
-}
-
+import { GreaterThanDateValidator, DateTimeMinutesAllowedValidator } from 'app/validators/allocation.validators';
 
 @Component({
-  selector: 'app-csveditor',
-  templateUrl: '../html/collects-scheduler.component.html',
-  styleUrls: ['../css/collects-scheduler.component.scss']
+    selector: 'app-csveditor',
+    templateUrl: '../html/collects-scheduler.component.html',
+    styleUrls: ['../css/collects-scheduler.component.scss']
 })
-
 export class CollectsShedulerComponent implements OnInit {
+    public form: FormGroup;
+    public cacheKey = 'CollectSchedulerComponent';
 
-  public form: FormGroup;
-  public cacheKey = 'CollectSchedulerComponent';
+    currentCollectGroupAllocations = [];
 
-  currentCollectGroupAllocations = []
+    currentTotalNumberOfPages: 0;
+    currentTotalCountOfRows: 0;
+    pageSettings = { currentPage: 1, currentRowsPerPage: 10 };
 
-  currentTotalNumberOfPages: 0
-  currentTotalCountOfRows: 0
-  pageSettings = { currentPage: 1, currentRowsPerPage: 10 }
-
-  constructor(
-    private formBuilder: FormBuilder,
-    private userService: UserService,
-    private service: CollectSchedulerService,
-    private datePipe: ISODatePipe,
-    private loggingService: LoggingService) {
-    this.userService.collectGroupChanged.subscribe(() => {
-      this.ngOnInit();
-    });
-  }
-
-  ngOnInit() {
-    this.getRows(this.pageSettings)
-  }
-
-  public get collectsArray(): FormArray {
-    return this.form.get('collects') as FormArray;
-  }
-
-  buildSingleForm(scheduler: any = null, copyId: boolean = false): FormGroup {
-    const form = this.formBuilder.group(
-      {
-        id: [scheduler && copyId ? scheduler.Id : 0],
-        dtBegin: [scheduler && scheduler.dtBegin ? this.datePipe.transform(new Date(scheduler.dtBegin), "yyyy-MM-ddTHH:mm"): null, [Validators.required]],
-        dtEnd: [scheduler && scheduler.dtEnd ? this.datePipe.transform(new Date(scheduler.dtEnd), "yyyy-MM-ddTHH:mm") : null, [Validators.required]],
-        name: [scheduler ? scheduler.Name : null, [Validators.required, Validators.maxLength(50), Validators.minLength(3)]],
-        collectId: [scheduler ? scheduler.CollectId : 1, [Validators.required, Validators.min(1), Validators.max(3)]]
-      },
-      { validator: GreaterThanDateValidator }
-    );
-
-    form.valueChanges
-      .pipe(debounceTime(1500))
-      .pipe(distinctUntilChanged((oldValue: any, newValue: any) => compareRows(oldValue, newValue)))
-      .subscribe(x => this.upload(form));
-
-    return form;
-
-  }
-
-  addEmptyRow(index: number = 0) {
-    this.collectsArray.insert(index, this.buildSingleForm());
-  }
-
-  removeRow(index: number) {
-    const toDeleteFormGroup = this.collectsArray.at(index);
-    if (toDeleteFormGroup.value.id > 0) {
-      this.service.deleteAllocation(this.userService.CurrentCollectGroup.GUID, toDeleteFormGroup.value.id)
-        .pipe(catchError((error: HttpErrorResponse) => this.handleGenericError(error)))
-        .subscribe(x => this.removeRowConfirm(index))
-    } else {
-      this.removeRowConfirm(index);
-    }
-  }
-
-  removeRowConfirm(index: number) {
-    this.collectsArray.removeAt(index);
-  }
-
-  copyRow(index: number) {
-    var row = this.collectsArray.at(index).value
-
-    this.collectsArray.insert(index, this.buildSingleForm({
-      CollectId: row.collectId,
-      Name: row.name,
-      dtBegin: row.dtBegin,
-      dtEnd: row.dtEnd,
-    }));
-  }
-  getRows(options: InfrastructurePaginator) {
-    this.service
-      .getAll(this.userService.CurrentCollectGroup.GUID, options.currentRowsPerPage, options.currentPage)
-      .pipe(catchError((error: HttpErrorResponse) => this.handleGenericError(error)))
-      .subscribe(response => {
-        this.currentCollectGroupAllocations = response.Results
-        this.currentTotalNumberOfPages = response.TotalNumberOfPages
-        this.currentTotalCountOfRows = response.TotalCount
-        this.form = this.formBuilder.group({
-          collects: this.formBuilder.array(this.currentCollectGroupAllocations ? this.currentCollectGroupAllocations.map(x => this.buildSingleForm(x, true)) : [])
-        });
-      })
-  }
-  upload(row: FormGroup) {
-    if (row.invalid) {
-      return;
-    }
-    if (row.value.id && row.value.id > 0) {
-      // update existing allocation
-      this.service
-        .updateAllocation(this.userService.CurrentCollectGroup.GUID, row.value.id, row.value)
-        .pipe(catchError((error: HttpErrorResponse) => (error.status === 409 ? this.handleConflict(error, row) : this.handleGenericError())))
-        .subscribe(x => this.loggingService.info(`Allocation with id '${row.value.id}' was updated for CollectGroup: ${this.userService.CurrentCollectGroup.Name}`));
-    } else {
-      // create a new allocation
-      this.service
-        .createAllocation(this.userService.CurrentCollectGroup.GUID, row.value)
-        .pipe(catchError((error: HttpErrorResponse) => (error.status === 409 ? this.handleConflict(error, row) : this.handleGenericError())))
-        .subscribe(x => {
-          this.loggingService.info(`Allocation with id '${x.Id}' was created for CollectGroup: ${this.userService.CurrentCollectGroup.Name}` )
-          row.patchValue({ id: x.Id }
-          )
+    constructor(
+        private formBuilder: FormBuilder,
+        private userService: UserService,
+        private service: CollectSchedulerService,
+        private datePipe: ISODatePipe,
+        private loggingService: LoggingService
+    ) {
+        this.userService.collectGroupChanged.subscribe(() => {
+            this.ngOnInit();
         });
     }
 
-  }
-  handleConflict(error: any, form: FormGroup) {
-    form.setErrors({ overlap: true });
-    return Observable.throw(error);
+    ngOnInit() {
+        this.getRows(this.pageSettings);
+    }
 
-  }
-  handleGenericError(error: any = null) {
-    console.log(error != null ? error : "generic error");
-    return Observable.throw(error != null ? error : "generic error");
+    public get collectsArray(): FormArray {
+        return this.form.get('collects') as FormArray;
+    }
 
-  }
+    buildSingleForm(scheduler: any = null, copyId: boolean = false): FormGroup {
+        const form = this.formBuilder.group(
+            {
+                id: [scheduler && copyId ? scheduler.Id : 0],
+                dtBegin: [
+                    scheduler && scheduler.dtBegin ? this.datePipe.transform(new Date(scheduler.dtBegin), 'yyyy-MM-ddTHH:mm') : null,
+                    [Validators.required, DateTimeMinutesAllowedValidator([0, 30])]
+                ],
+                dtEnd: [
+                    scheduler && scheduler.dtEnd ? this.datePipe.transform(new Date(scheduler.dtEnd), 'yyyy-MM-ddTHH:mm') : null,
+                    [Validators.required, DateTimeMinutesAllowedValidator([0, 30])]
+                ],
+                name: [scheduler ? scheduler.Name : null, [Validators.required, Validators.maxLength(50), Validators.minLength(3)]],
+                collectId: [scheduler ? scheduler.CollectId : 1, [Validators.required, Validators.min(1), Validators.max(3)]]
+            },
+            { validator: GreaterThanDateValidator }
+        );
+
+        form.valueChanges
+            .pipe(debounceTime(1500))
+            .pipe(distinctUntilChanged((oldValue: any, newValue: any) => compareRows(oldValue, newValue)))
+            .subscribe(x => this.upload(form));
+
+        return form;
+    }
+
+    addEmptyRow(index: number = 0) {
+        this.collectsArray.insert(index, this.buildSingleForm());
+    }
+
+    removeRow(index: number) {
+        const toDeleteFormGroup = this.collectsArray.at(index);
+        if (toDeleteFormGroup.value.id > 0) {
+            this.service
+                .deleteAllocation(this.userService.CurrentCollectGroup.GUID, toDeleteFormGroup.value.id)
+                .pipe(catchError((error: HttpErrorResponse) => this.handleGenericError(error)))
+                .subscribe(x => this.removeRowConfirm(index));
+        } else {
+            this.removeRowConfirm(index);
+        }
+    }
+
+    removeRowConfirm(index: number) {
+        this.collectsArray.removeAt(index);
+    }
+
+    copyRow(index: number) {
+        var row = this.collectsArray.at(index).value;
+
+        this.collectsArray.insert(
+            index,
+            this.buildSingleForm({
+                CollectId: row.collectId,
+                Name: row.name,
+                dtBegin: row.dtBegin,
+                dtEnd: row.dtEnd
+            })
+        );
+    }
+    getRows(options: InfrastructurePaginator) {
+        this.service
+            .getAll(this.userService.CurrentCollectGroup.GUID, options.currentRowsPerPage, options.currentPage)
+            .pipe(catchError((error: HttpErrorResponse) => this.handleGenericError(error)))
+            .subscribe(response => {
+                this.currentCollectGroupAllocations = response.Results;
+                this.currentTotalNumberOfPages = response.TotalNumberOfPages;
+                this.currentTotalCountOfRows = response.TotalCount;
+                this.form = this.formBuilder.group({
+                    collects: this.formBuilder.array(
+                        this.currentCollectGroupAllocations
+                            ? this.currentCollectGroupAllocations.map(x => this.buildSingleForm(x, true))
+                            : []
+                    )
+                });
+            });
+    }
+    upload(row: FormGroup) {
+        if (row.invalid) {
+            return;
+        }
+        if (row.value.id && row.value.id > 0) {
+            // update existing allocation
+            this.service
+                .updateAllocation(this.userService.CurrentCollectGroup.GUID, row.value.id, row.value)
+                .pipe(
+                    catchError((error: HttpErrorResponse) =>
+                        error.status === 409 ? this.handleConflict(error, row) : this.handleGenericError()
+                    )
+                )
+                .subscribe(x =>
+                    this.loggingService.info(
+                        `Allocation with id '${row.value.id}' was updated for CollectGroup: ${this.userService.CurrentCollectGroup.Name}`
+                    )
+                );
+        } else {
+            // create a new allocation
+            this.service
+                .createAllocation(this.userService.CurrentCollectGroup.GUID, row.value)
+                .pipe(
+                    catchError((error: HttpErrorResponse) =>
+                        error.status === 409 ? this.handleConflict(error, row) : this.handleGenericError()
+                    )
+                )
+                .subscribe(x => {
+                    this.loggingService.info(
+                        `Allocation with id '${x.Id}' was created for CollectGroup: ${this.userService.CurrentCollectGroup.Name}`
+                    );
+                    row.patchValue({ id: x.Id });
+                });
+        }
+    }
+    handleConflict(error: any, form: FormGroup) {
+        form.setErrors({ overlap: true });
+        return Observable.throw(error);
+    }
+    handleGenericError(error: any = null) {
+        console.log(error != null ? error : 'generic error');
+        return Observable.throw(error != null ? error : 'generic error');
+    }
+
+    hasErrors(): boolean {
+        return this.form &&  this.collectsArray &&  this.collectsArray.controls.some((formGroup: FormGroup) => {
+            return formGroup.errors !== null || Object.keys(formGroup.controls).some(key => formGroup.get(key).errors !== null);
+        });
+    }
 }
