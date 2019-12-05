1293f7cfc5ef3d70bd5172a6eaf2570cea6f61dd
Maarten Vergouwe <mvergouwe85@gmail.com>
2019-12-05 15:01:39 +0100

    Make JSON stringify for Dates always ISO UTC string

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,17 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { sprintf } from 'sprintf-js';
+
+Date.prototype.toJSON = function () {
+    return sprintf("%04i-%02i-%02iT%02i:%02i:%02i.%03i",
+        this.getUTCFullYear(), this.getUTCMonth()+1, this.getUTCDate(),
+        this.getUTCHours(), this.getUTCMinutes(), this.getUTCSeconds(), this.getUTCMilliseconds()) + "Z";
+}
 
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+
