b0c5e61cf328ca8487b3b50806a3de19291faec5
Mike <mike@givtapp.net>
2020-06-30 12:11:24 +0200

    add the logstash api key in environment + also log from local instead of just prod or debug

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:5000',
+  logstashApiKey: '73b6d8f0-132f-45ff-a8cf-6654ffee1922',
+  logstashTag: 'GivtDashboard.Local'
+
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.givtapp.net',
+  logstashApiKey: '73b6d8f0-132f-45ff-a8cf-6654ffee1922',
+  logstashTag: 'GivtDashboard.Production'
 };
