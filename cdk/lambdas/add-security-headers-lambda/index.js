a4e7980e9e333020720e3c0f7fefccc893b633b0
Anthony <svansay@pansay.com>
2021-06-29 16:25:35 +0200

    final fixes for security headers

--- cdk/lambdas/add-security-headers-lambda/index.js
+++ cdk/lambdas/add-security-headers-lambda/index.js
@@ -6,8 +6,9 @@ exports.handler = (event, context, callback) => {
     const headers = response.headers;
 
     //Set new headers 
+    headers['permissions-policy'] = [{ key: 'Permissions-Policy', value: `fullscreen=(self); layout-animations=(self); legacy-image-formats=(self);` }]
     headers['strict-transport-security'] = [{ key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubdomains; preload' }];
-    headers['content-security-policy'] = [{ key: 'Content-Security-Policy', value: "default-src 'none';font-src 'self' use.fontawesome.com cdnjs.cloudflare.com; img-src 'self'; script-src 'self' gstatic.com googletagmanager.com ; style-src 'self' use.fontawesome.com cdnjs.cloudflare.com; object-src 'none';" }];
+    headers['content-security-policy'] = [{ key: 'Content-Security-Policy', value: "connect-src 'self' givt-debug-api.azurewebsites.net api.givtapp.net www.google-analytics.com; default-src 'none';font-src 'self' use.fontawesome.com cdnjs.cloudflare.com; img-src 'self' www.google-analytics.com; script-src  www.gstatic.com www.googletagmanager.com www.google-analytics.com 'unsafe-eval' 'self'; style-src 'unsafe-inline' 'self' use.fontawesome.com cdnjs.cloudflare.com; object-src 'none';" }];
     headers['x-content-type-options'] = [{ key: 'X-Content-Type-Options', value: 'nosniff' }];
     headers['x-frame-options'] = [{ key: 'X-Frame-Options', value: 'DENY' }];
     headers['x-xss-protection'] = [{ key: 'X-XSS-Protection', value: '1; mode=block' }];
