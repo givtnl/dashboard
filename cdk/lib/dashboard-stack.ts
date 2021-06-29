5400abde4ca776272324bddc73ae839215e7d102
Anthony <svansay@pansay.com>
2021-06-29 11:55:15 +0200

    make sure the cloudfront can also be created for a demo environment, and add the lambda for the security headers

--- cdk/lib/dashboard-stack.ts
+++ cdk/lib/dashboard-stack.ts
@@ -1,18 +1,20 @@
-import { Certificate } from "@aws-cdk/aws-certificatemanager";
+import { Certificate, ICertificate } from "@aws-cdk/aws-certificatemanager";
 import {
-    AllowedMethods,
     CacheCookieBehavior,
-    CachedMethods,
     CacheHeaderBehavior,
     CachePolicy,
     CacheQueryStringBehavior,
     Distribution,
     HttpVersion,
+    ICachePolicy,
+    LambdaEdgeEventType,
     OriginAccessIdentity,
     PriceClass,
     ViewerProtocolPolicy,
 } from "@aws-cdk/aws-cloudfront";
 import { S3Origin } from "@aws-cdk/aws-cloudfront-origins";
+import { EdgeFunction } from "@aws-cdk/aws-cloudfront/lib/experimental";
+import { Code, Runtime } from "@aws-cdk/aws-lambda";
 import {
     BlockPublicAccess,
     Bucket,
@@ -22,21 +24,21 @@ import {
 import { BucketDeployment, CacheControl, Source, StorageClass } from '@aws-cdk/aws-s3-deployment'
 import { StringParameter } from "@aws-cdk/aws-ssm";
 import * as cdk from "@aws-cdk/core";
-import { CfnParameter, Duration } from "@aws-cdk/core";
+import { Duration } from "@aws-cdk/core";
 
 export class DashboardStack extends cdk.Stack {
+
+    public certificate: ICertificate;
+    public cachePolicy: ICachePolicy;
+    public securityHeadersFunction: EdgeFunction;
+
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
-        // The code that defines your stack goes here
-        var webhostingBucket = new Bucket(this, "DashboardWebhostingBucket", {
-            accessControl: BucketAccessControl.PRIVATE,
-            enforceSSL: true,
-            publicReadAccess: false,
-            blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
-            encryption: BucketEncryption.S3_MANAGED,
-        });
 
-        var certificate = Certificate.fromCertificateArn(
+        var environmentName = process.env?.EnvironmentName ?? 'development';
+        var isProduction = environmentName.toLowerCase() == "production";
+
+        this.certificate = Certificate.fromCertificateArn(
             this,
             "Certificate",
             StringParameter.valueForStringParameter(
@@ -44,20 +46,10 @@ export class DashboardStack extends cdk.Stack {
                 "DashboardCertificateArn"
             )
         );
-
-        var cloudFrontOriginAccessIdentity = new OriginAccessIdentity(
-            this,
-            "DashboardAccessIdentity",
-            {
-                comment:
-                    "The identity that will be used by cloudfront to access our webhosting bucket",
-            }
-        );
-        webhostingBucket.grantRead(cloudFrontOriginAccessIdentity);
-
-        var cachePolicy = new CachePolicy(this, "DashboardCachePolicy", {
+        this.cachePolicy = new CachePolicy(this, "DashboardCachePolicy", {
             queryStringBehavior: CacheQueryStringBehavior.all(),
             cookieBehavior: CacheCookieBehavior.all(),
+            comment: 'Default Cache policy for the dashboard',
             headerBehavior: CacheHeaderBehavior.allowList(
                 "Access-Control-Request-Headers",
                 "Access-Control-Request-Method",
@@ -66,16 +58,51 @@ export class DashboardStack extends cdk.Stack {
             enableAcceptEncodingBrotli: true,
             enableAcceptEncodingGzip: true,
         });
+        this.securityHeadersFunction = new EdgeFunction(this, 'MyFunction', {
+            runtime: Runtime.NODEJS_12_X,
+            handler: 'index.handler',
+            functionName:'add-security-headers-function',
+            description:'Adds security headers to the responses from S3',
+            code: Code.fromAsset('./lambdas/add-security-headers-lambda.js')
+        });
+        this.deploy(isProduction ? 'cloud.givtapp.net' : 'clouddebug.givtapp.net', environmentName, '../dist');
+        if (isProduction) {
+            // deploy the demo environment
+            this.deploy('demo.givtapp.net', 'demo', '../dist-demo');
+        }
+    }
+
+    private deploy(domainName: string, environmentName: string, folderToDeploy: string): void {
+        // The code that defines your stack goes here
+        var webhostingBucket = new Bucket(this, `DashboardWebhostingBucket${environmentName}`, {
+            accessControl: BucketAccessControl.PRIVATE,
+            enforceSSL: true,
+            publicReadAccess: false,
+            blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+            encryption: BucketEncryption.S3_MANAGED,
+        });
+
+        var cloudFrontOriginAccessIdentity = new OriginAccessIdentity(
+            this,
+            `DashboardAccessIdentity${environmentName}`,
+            {
+                comment:
+                    "The identity that will be used by cloudfront to access our webhosting bucket",
+            }
+        );
+
+        webhostingBucket.grantRead(cloudFrontOriginAccessIdentity);
+
         var cloudFrontDistribution = new Distribution(
             this,
-            "DashboardCloudFrontDistribution",
+            `DashboardCloudFrontDistribution${environmentName}`,
             {
                 enabled: true,
                 priceClass: PriceClass.PRICE_CLASS_100,
                 comment:
                     "The dashboard cloudfront distribution which will serve the dashboard version one to our users",
                 httpVersion: HttpVersion.HTTP2,
-                certificate: certificate,
+                certificate: this.certificate,
                 defaultRootObject: 'index.html',
                 errorResponses: [
                     {
@@ -89,29 +116,16 @@ export class DashboardStack extends cdk.Stack {
                         responsePagePath: "/index.html",
                     },
                 ],
-                domainNames: [
-                    process.env.EnvironmentName?.toLowerCase() ==
-                        "development"
-                        ? "clouddebug.givtapp.net"
-                        : "cloud.givtapp.net",
-                ],
-                additionalBehaviors: {
-                    '/demo/*': {
-                        origin: new S3Origin(webhostingBucket, {
-                            originPath: '/demo',
-                            originAccessIdentity: cloudFrontOriginAccessIdentity
-                        }),
-                        compress: true,
-                        viewerProtocolPolicy:
-                            ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-                        cachePolicy: cachePolicy
-                    }
-                },
+                domainNames: [domainName],
                 defaultBehavior: {
+                    edgeLambdas: [{
+                        eventType: LambdaEdgeEventType.ORIGIN_RESPONSE,
+                        functionVersion: this.securityHeadersFunction.currentVersion
+                    }],
                     compress: true,
                     viewerProtocolPolicy:
                         ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-                    cachePolicy: cachePolicy,
+                    cachePolicy: this.cachePolicy,
                     origin: new S3Origin(webhostingBucket, {
                         originAccessIdentity: cloudFrontOriginAccessIdentity,
                     }),
@@ -119,21 +133,12 @@ export class DashboardStack extends cdk.Stack {
             }
         );
 
-        new BucketDeployment(this, "StaticWebsiteDeployment", {
+        new BucketDeployment(this, `StaticWebsiteDeployment${environmentName}`, {
             cacheControl: [CacheControl.maxAge(Duration.days(31))],
             destinationBucket: webhostingBucket,
             storageClass: StorageClass.ONEZONE_IA,
             distribution: cloudFrontDistribution,
-            sources: [Source.asset('../dist')]
-        });
-
-        new BucketDeployment(this, "StaticDemoWebsiteDeployment", {
-            cacheControl: [CacheControl.maxAge(Duration.days(31))],
-            destinationBucket: webhostingBucket,
-            storageClass: StorageClass.ONEZONE_IA,
-            distribution: cloudFrontDistribution,
-            destinationKeyPrefix: 'demo/',
-            sources: [Source.asset('../dist-demo')]
+            sources: [Source.asset(folderToDeploy)]
         });
     }
 }
