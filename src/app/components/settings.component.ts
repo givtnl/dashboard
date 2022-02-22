9cf2ffa9c48df4324fd87b63bcacfe82680ef2b0
Michiel Verkest <michiel@Michiels-MacBook-Pro.local>
2022-02-22 14:24:53 +0100

    change email address in integration page in settings

--- src/app/components/settings.component.ts
+++ src/app/components/settings.component.ts
@@ -1,120 +1,127 @@
-import { DataService } from "../services/data.service";
-import { Component, OnInit, AfterViewInit } from "@angular/core";
-import { LangChangeEvent, TranslateService } from "ng2-translate";
-import { UserService } from "../services/user.service";
-import { isNullOrUndefined } from "util";
-import { LoggingService } from "app/services/logging.service";
+import {DataService} from "../services/data.service";
+import {Component, OnInit, AfterViewInit} from "@angular/core";
+import {LangChangeEvent, TranslateService} from "ng2-translate";
+import {UserService} from "../services/user.service";
+import {isNullOrUndefined} from "util";
+import {LoggingService} from "app/services/logging.service";
 
 
 @Component({
-	selector: "settings",
-	templateUrl: '../html/settings.component.html',
-	styleUrls: ['../css/settings.component.css'],
+    selector: "settings",
+    templateUrl: '../html/settings.component.html',
+    styleUrls: ['../css/settings.component.css'],
 })
 
 export class SettingsComponent implements OnInit {
-	private _firstDay: number = 0;
-	private days = [];
-	private selectedQRLanguage: String;
-	public isSettingsDetailVisible = false;
-	public isDeepLinkVisible = false;
-	public isQRCodeSettingVisible = false;
-	public currentCollectGroup;
+    private _firstDay: number = 0;
+    private days = [];
+    private selectedQRLanguage: String;
+    public isSettingsDetailVisible = false;
+    public isDeepLinkVisible = false;
+    public isQRCodeSettingVisible = false;
+    public currentCollectGroup;
 
-	requestMediumIdTitle: String;
-	requestMediumIdBody: String;
-	requestMediumIdManual: String;
+    requestMediumIdTitle: String;
+    requestMediumIdBody: String;
+    requestMediumIdManual: String;
 
 
-	public availableLanguages: any
+    public availableLanguages: any
 
-	constructor(        
-		private loggingService: LoggingService,
-		private dataService: DataService, 
-		private translateService: TranslateService, 
-		private userService: UserService) {
-		this.loadTerms();
-		this.currentCollectGroup = userService.CurrentCollectGroup;
-		this.loadConnectWithGivt();
-		this.loadQRLanguages();
-	}
-	ngOnInit() {
-		window.scrollTo(0, 0);
-		let localDay = this.dataService.getData("FirstDayOfWeek");
-		if (!isNaN(localDay)) {
-			this.firstDay = localDay;
-		}
+    constructor(
+        private loggingService: LoggingService,
+        private dataService: DataService,
+        private translateService: TranslateService,
+        private userService: UserService) {
+        this.loadTerms();
+        this.currentCollectGroup = userService.CurrentCollectGroup;
+        this.loadConnectWithGivt();
+        this.loadQRLanguages();
+    }
 
-		this.translateService.onLangChange.subscribe((event: LangChangeEvent) => {
-			this.loadTerms();
-			this.loadConnectWithGivt();
-			this.loadQRLanguages();
-		});
+    ngOnInit() {
+        window.scrollTo(0, 0);
+        let localDay = this.dataService.getData("FirstDayOfWeek");
+        if (!isNaN(localDay)) {
+            this.firstDay = localDay;
+        }
 
-		if (!isNullOrUndefined(navigator.language)) {
-			this.selectedQRLanguage = navigator.language.substring(0, 2)
-		}
-	}
+        this.translateService.onLangChange.subscribe((event: LangChangeEvent) => {
+            this.loadTerms();
+            this.loadConnectWithGivt();
+            this.loadQRLanguages();
+        });
 
-	get firstDay(): number {
-		return this._firstDay;
-	}
+        if (!isNullOrUndefined(navigator.language)) {
+            this.selectedQRLanguage = navigator.language.substring(0, 2)
+        }
+    }
 
-	set firstDay(value: number) {
-		this._firstDay = value;
-		this.dataService.writeData("FirstDayOfWeek", value, true);
-	}
-	
+    get firstDay(): number {
+        return this._firstDay;
+    }
 
-	private loadQRLanguages() {
-		this.availableLanguages = [
-			{
-				name: this.translateService.instant("LanguageEN"),
-				value: "en"
-			},
-			{
-				name: this.translateService.instant("LanguageNL"),
-				value: "nl"
-			}
-		]
-	}
+    set firstDay(value: number) {
+        this._firstDay = value;
+        this.dataService.writeData("FirstDayOfWeek", value, true);
+    }
 
-	private loadTerms() {
-		this.days.length = 0;
-		this.days.push(this.translateService.instant("Zondag").toString());
-		this.days.push(this.translateService.instant("Monday").toString());
-		this.days.push(this.translateService.instant("Tuesday").toString());
-		this.days.push(this.translateService.instant("Wednesday").toString());
-		this.days.push(this.translateService.instant("Thursday").toString());
-		this.days.push(this.translateService.instant("Friday").toString());
-		this.days.push(this.translateService.instant("Saturday").toString());
-	}
 
-	private loadConnectWithGivt() {
-		this.requestMediumIdTitle = this.translateService.instant("RequestMediumIdTitle").toString();
-		this.requestMediumIdTitle = this.requestMediumIdTitle.replace("{0}", this.currentCollectGroup.Name);
-		this.requestMediumIdTitle = this.requestMediumIdTitle.replace("{1}", this.currentCollectGroup.Namespace);
-		this.requestMediumIdBody = this.translateService.instant("RequestMediumIdBody");
-		this.requestMediumIdManual = this.translateService.instant("RequestMediumIdManual");
-	}
+    private loadQRLanguages() {
+        this.availableLanguages = [
+            {
+                name: this.translateService.instant("LanguageEN"),
+                value: "en"
+            },
+            {
+                name: this.translateService.instant("LanguageNL"),
+                value: "nl"
+            }
+        ]
+    }
 
-	goBack() {
-		window.history.back();
-	}
+    private loadTerms() {
+        this.days.length = 0;
+        this.days.push(this.translateService.instant("Zondag").toString());
+        this.days.push(this.translateService.instant("Monday").toString());
+        this.days.push(this.translateService.instant("Tuesday").toString());
+        this.days.push(this.translateService.instant("Wednesday").toString());
+        this.days.push(this.translateService.instant("Thursday").toString());
+        this.days.push(this.translateService.instant("Friday").toString());
+        this.days.push(this.translateService.instant("Saturday").toString());
+    }
 
-	goBackToSettings() {
-		this.isSettingsDetailVisible = false;
-	}
-	copyNamespace() {
-		let copyText = document.getElementById("cg-namespace") as HTMLInputElement;
-		copyText.focus();
-		copyText.select();
-		document.execCommand("Copy");
-	}
-	saveQRCodeLanguage() {
-		this.dataService.writeData("SelectedQRCodeLanguage", this.selectedQRLanguage, true)
-		var button = document.getElementById("QRSET")
-		button.classList.add("qr-check-fade");
-		this.loggingService.info(`QR Code language changed to ${this.selectedQRLanguage} for collectgroup: ${this.userService.CurrentCollectGroup.Name}`)
-	}
+    private loadConnectWithGivt() {
+        this.requestMediumIdTitle = this.translateService.instant("RequestMediumIdTitle").toString();
+        this.requestMediumIdTitle = this.requestMediumIdTitle.replace("{0}", this.currentCollectGroup.Name);
+        this.requestMediumIdTitle = this.requestMediumIdTitle.replace("{1}", this.currentCollectGroup.Namespace);
+        if (this.userService && this.userService.CurrentCollectGroup.Country !== 'US') {
+            this.requestMediumIdBody = this.translateService.instant("RequestMediumIdBody");
+        } else {
+            this.requestMediumIdBody = this.translateService.instant("RequestMediumIdBodyUS");
+        }
+        this.requestMediumIdManual = this.translateService.instant("RequestMediumIdManual");
+    }
+
+    goBack() {
+        window.history.back();
+    }
+
+    goBackToSettings() {
+        this.isSettingsDetailVisible = false;
+    }
+
+    copyNamespace() {
+        let copyText = document.getElementById("cg-namespace") as HTMLInputElement;
+        copyText.focus();
+        copyText.select();
+        document.execCommand("Copy");
+    }
+
+    saveQRCodeLanguage() {
+        this.dataService.writeData("SelectedQRCodeLanguage", this.selectedQRLanguage, true)
+        var button = document.getElementById("QRSET")
+        button.classList.add("qr-check-fade");
+        this.loggingService.info(`QR Code language changed to ${this.selectedQRLanguage} for collectgroup: ${this.userService.CurrentCollectGroup.Name}`)
+    }
 }
