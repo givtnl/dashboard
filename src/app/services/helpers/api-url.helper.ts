c920761fe48bbba25f4ded6475b4c36c0d05e9a8
Alex <alex@givtapp.net>
2022-09-08 16:00:11 +0200

    Checked/corrected debug/production URLs, added debug logic

--- src/app/services/helpers/api-url.helper.ts
+++ src/app/services/helpers/api-url.helper.ts
@@ -5,12 +5,17 @@ export function getApiUrl(): string {
      * Based on the hostname, the backend is set for eu (givtapp.net) or us (givt.app)
      * For local dev apiUrl remains used
      */
-
     if(environment.production) {
         if(window.location.hostname.endsWith('givt.app'))
             return environment.apiUrlUS;
         else
             return environment.apiUrlEU;
     }
-    return environment.apiUrl;
+
+    if (window.location.hostname.endsWith('givt.app'))
+        return environment.apiUrlUS;
+    else if (window.location.hostname.endsWith('givtapp.net'))
+        return environment.apiUrlEU;
+    else 
+        return environment.apiUrl;
 }
--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
+  // for testing purposes
   apiUrl: 'http://localhost:5000',
-  apiUrlEU: '',
-  apiUrlUS: '',
+  apiUrlEU: 'http://localhost:5000',
+  apiUrlUS: 'http://localhost:5001',
   logstashApiKey: '73b6d8f0-132f-45ff-a8cf-6654ffee1922',
   logstashTag: 'GivtDashboard.Local'
-
 };
